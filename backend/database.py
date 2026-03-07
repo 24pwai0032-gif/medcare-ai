a7e25cbba810f05de7f62f7d59f73b77bbd19a2f
24pwai0032-gif <hassanayaxy@gmail.com>
2026-03-07 07:35:35 -0800

    Setting Default Database for GCP Deploy - SQlite

--- backend/database.py
+++ backend/database.py
@@ -1,25 +1,20 @@
 # backend/database.py
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
+# GCP pe SQLite, local pe PostgreSQL
 DATABASE_URL = os.getenv(
     "DATABASE_URL",
-    "postgresql://postgres:postgres@localhost:5432/medcare_db"
+    "sqlite:///./medcare.db"
 )
 
-engine = create_engine(DATABASE_URL)
+# SQLite fix
+connect_args = {"check_same_thread": False} if "sqlite" in DATABASE_URL else {}
 
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
-
+engine = create_engine(DATABASE_URL, connect_args=connect_args)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 def get_db():
